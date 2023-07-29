e43506a4852c0ef32241fbb85a31f7412e89df15
Alexander Masser <alexandermasser24@gmail.com>
2023-07-29 16:49:34 +0200

    Deine Heutigen änderunen

--- res/entity.hpp
+++ res/entity.hpp
@@ -1,8 +1,45 @@
 
 
 namespace dnd{
-  class entity{
-    int stat;
+  
+  enum class dndClass : int
+  {
+    classBarbarian,
+    classBard,
+    classCleric,
+    classDruid,
+    classFighter,
+    classMonk,
+    classPaladin,
+    classRanger,
+    classRogue,
+    classSorcerer,
+    classWarlock,
+    classWizard
+  };
+
+  enum class dndRace : int
+  {
+    raceDragonborn,
+    raceDwarf,
+    raceElf,
+    raceGnome,
+    raceHalf_elf,
+    raceHalfling,
+    raceHalf_orc,
+    raceHuman,
+    raceTiefling
+  };
+
+  class entity
+  {
+    int m_strength = 0;
+    int m_dexterity = 0;
+    int m_constitution = 0;
+    int m_intelligence = 0;
+    int m_wisdom = 0;
+    int m_charisma = 0;
+    int m_level = 0;
     
   };
 }
